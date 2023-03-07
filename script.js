d5679b6f2a2898439e5ce99017117ae883afe878
Mac <mac@Macs-MacBook-Air.local>
2023-03-07 13:46:56 +0630

    done

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
+let language_button = document.querySelector('.language_button');
+let language_list = document.querySelector('.language_list');
+
+language_button.onclick = () => {
+    language_list.classList.toggle('active');   
+}
 
 menu.onclick = ()=>{
     menu.classList.toggle('fa-times');
